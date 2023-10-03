00687dc0d8c1c4eff1855bea0c3833ab5ec0f7df
molul <luismorcillo@gmail.com>
2023-10-03 21:27:19 +0200

    Padding in content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,50 +4,9 @@ import KeyFeatures from "./Content/KeyFeatures";
 const Content = () => {
   return (
     <div className="flex flex-1 justify-center w-full p-4 lg:p-12 mt-20 lg:mt-0">
-      <div className="w-full max-w-6xl space-y-16">
+      <div className="w-full max-w-6xl space-y-16 py-4">
         <Cover />
         <KeyFeatures />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
       </div>
     </div>
   );
