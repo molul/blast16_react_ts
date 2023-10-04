66f62cd1bd10d678f1c3b6e9ef2d6d5894e012c3
molul <luismorcillo@gmail.com>
2023-10-04 23:31:23 +0200

    Screenshots wouldn't let compile

--- src/components/sections/Screenshots.tsx
+++ src/components/sections/Screenshots.tsx
@@ -1,35 +1,5 @@
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-import ImageGallery from "react-image-gallery";
-
-const divStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundSize: "cover",
-  height: "auto",
-  width: "100%",
-};
-
 const Screenshots = () => {
-  const slideshowPath = "/img/screenshots/";
-  const slideImages = [];
-  for (let i = 0; i < 9; i++) {
-    slideImages.push({
-      original: slideshowPath + (i + 1) + ".webp",
-      thumbnail: slideshowPath + (i + 1) + ".webp",
-    });
-  }
-
-  return (
-    <div className="slide-container">
-      {slideImages.map((slideImage, index) => (
-        <div key={index}>
-          <ImageGallery items={slideImage} />
-        </div>
-      ))}
-    </div>
-  );
+  return <div className="slide-container"></div>;
 };
 
 export default Screenshots;
