6047cb684616f949cd9dc9d58b22e1c3b9cdc0c0
molul <luismorcillo@gmail.com>
2023-10-04 08:54:01 +0200

    Actualizar KeyFeatures.tsx

--- src/components/sections/KeyFeatures.tsx
+++ src/components/sections/KeyFeatures.tsx
@@ -8,7 +8,7 @@ const KeyFeatures = () => {
       <div className="	px-6 break-words">
         <ul className="list-disc">
           {features.key.map((feat, index) => (
-            <li key={index}>{<>feat</>}</li>
+            <li key={index}>{<>{feat}</>}</li>
           ))}
         </ul>
       </div>
