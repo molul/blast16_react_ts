ddbe396cac9735e8d1e838b6743582d51405833a
molul <luismorcillo@gmail.com>
2023-10-05 09:09:56 +0200

    Actualizar Subtitle.tsx

---
+++ src/components/Subtitle.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  text: string;
+}
+
+const Subitle = ({ text }: Props) => {
+  return <h2 className="font-bold text-2xl uppercase text-center">{text}</h2>;
+};
+
+export default Subitle;
