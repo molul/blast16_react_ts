ca59908519d7f1bafecfbd8083d833b3fa7a9b4c
molul <luismorcillo@gmail.com>
2023-10-06 09:22:50 +0200

    Actualizar OtherFeatures.tsx

--- src/components/sections/OtherFeatures.tsx
+++ src/components/sections/OtherFeatures.tsx
@@ -5,7 +5,7 @@ const OtherFeatures = () => {
   return (
     <section className="space-y-8 ">
       <Title text="Other features" />
-      <div className="	pl-8 break-words bg-black bg-opacity-50 p-4 rounded-lg">
+      <div className=" break-words bg-black bg-opacity-50 p-4 rounded-lg">
         <div className="space-y-2">
           {features.other.map((feat, index) => (
             <div key={index} className="py-2">{feat}</div>
