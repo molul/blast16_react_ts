35d33eba06051d7d181d27f2578a646bd9f61e8f
molul <luismorcillo@gmail.com>
2023-10-06 09:02:16 +0200

    Actualizar KeyFeatures.tsx

--- src/components/sections/KeyFeatures.tsx
+++ src/components/sections/KeyFeatures.tsx
@@ -6,7 +6,7 @@ const KeyFeatures = () => {
     <section className="space-y-8 ">
       <Title text="Key features" />
 
-      <div className="	pl-8 break-words bg-black bg-opacity-50 p-4 rounded-lg">
+      <div className="break-words bg-black bg-opacity-50 p-4 rounded-lg">
         <div className="divide-y divide-white">
           {features.key.map((feat, index) => (
             <div key={index}>{feat}</div>
