a2e3ed8436b0dad4ef7c52f8973206de73244a0b
molul <luismorcillo@gmail.com>
2023-10-06 18:00:33 +0200

    Footer layout changed on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,20 +5,22 @@ import YoutubeIconLink from "./menus/YoutubeIconLink";
 const Footer = () => {
   return (
     <div className="bg-black bg-opacity-80 p-8 space-y-4">
-      <div className="grid grid-cols-2 md:flex gap-4 items-center md:flex-row justify-center space-x-0 md:space-x-8 text-center">
-        {footerItems.map((item, index) => {
-          return (
-            <a
-              key={index}
-              className="p-2 uppercase text-white hover:text-primary"
-              href={item.url}
-              target={item.target}
-            >
-              {item.text}
-            </a>
-          );
-        })}
-        <div className="flex items-center gap-6  md:gap-10 justify-center">
+      <div className="grid grid-rows-2 md:flex gap-4 items-center md:flex-row justify-center space-x-0 md:space-x-8 text-center">
+        <div className="grid grid-cols-3">
+          {footerItems.map((item, index) => {
+            return (
+              <a
+                key={index}
+                className="p-2 uppercase text-white hover:text-primary"
+                href={item.url}
+                target={item.target}
+              >
+                {item.text}
+              </a>
+            );
+          })}
+        </div>
+        <div className="block flex items-center gap-6  md:gap-10 justify-center">
           <TwitterIconLink menu="footer" />
           <YoutubeIconLink menu="footer" />
         </div>
